80db4e42f1923b9204622d03876db6bc636fbb05
rose <hyobinkim@gmail.com>
2023-02-18 03:18:02 +0900

    Add ~7-Convolutional-Neural-Network/Ex02.py

---
+++ 7-Convolutional-Neural-Network/Ex01.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import numpy as np
+from common.util import im2col
+sys.path.append(os.pardir)
+
+x1 = np.random.rand(1, 3, 7, 7)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape)
+
+x2 = np.random.rand(10, 3, 7, 7)
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)
